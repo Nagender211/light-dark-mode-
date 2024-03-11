c69d0daca7f00617c816d4f5c1e274eade94a732
Nagender211 <dnagender2019@gmail.com>
2024-03-11 08:45:31 +0530

    added the html,css,javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+const toggleSwitch=document.querySelector('input[type="checkbox"]');
+const nav=document.getElementById('nav');
+const toggleIcon=document.getElementById('toggle-icon');
+const image1=document.getElementById('image1');
+const image2=document.getElementById('image2');
+const image3=document.getElementById('image3');
+const textBox=document.getElementById('text-box');
+
+
+function imageMode(){
+    image1.src=`img/undraw_proud_coder_${color}.svg`;
+    image2.src=`img/undraw_feeling_proud_${color}.svg`;
+    image3.src=`img/undraw_conceptual_idea_${color}.svg`;
+    
+}
+
+
+
+
+function darkMode(){
+    nav.style.backgroundColor='rgb(0 0 0 /50%)';
+    textBox.style.backgroundColor='rgb(255 255 255 /50%)';
+    toggleIcon.children[0].textContent='Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    imageMode('dark');
+}
+
+function lightMode(){
+    nav.style.backgroundColor='rgb(255 255 255 /50%)';
+    textBox.style.backgroundColor='rgb(0 0 0 /50%)';
+    toggleIcon.children[0].textContent='Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    imageMode('light');
+}
+
+function themSwitch(event){
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+        localStorage.setItem('theme','dark');
+        darkMode();
+    }else{
+        document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme','light');
+        lightMode();
+    }
+    
+   
+}
+
+toggleSwitch.addEventListener('change',themSwitch);
+
+let currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme',currentTheme);
+    if(currentTheme ==='dark'){
+        toggleSwitch.checked=true;
+        darkMode();
+    }
+
+}
